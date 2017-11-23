6251010848c52484c0f345f449031b7074e971d7
harlott <marcoallott@gmail.com>
2017-11-23 06:52:06 +0100

    test storage instance having custom storages map

--- test/Storage.js
+++ test/Storage.js
@@ -1,10 +1,12 @@
 import { buildCustomStorage } from '../src/Storage'
 import { buildCustomStoragesMap } from '../src/Storage'
+import Storage from '../src/Storage'
 
 const expect = require('chai').expect
 
 describe('Storage', function() {
   let newStorage = buildCustomStorage('fileSystem', ()=>{}, ()=>{}, ()=>{})
+  let customStoragesMap = buildCustomStoragesMap('fileSystem', newStorage)
   describe('#buildCustomStorage()', function() {
     it('should build the custom storage', function() {
         expect(newStorage).to.have.property('fileSystem')
@@ -13,11 +15,16 @@ describe('Storage', function() {
     });
   });
   describe('#buildCustomStoragesMap()', function() {
-    let customStoragesMap = buildCustomStoragesMap('fileSystem', newStorage)
     it('should build the custom storage map', function() {
         expect(customStoragesMap).to.have.property('fileSystem')
         expect(customStoragesMap).to.have.property('cookie')
         expect(customStoragesMap.fileSystem.setValue).to.be.a('function')
     });
   });
+  describe('#Storage', function(){
+    it('should add custom storage to Storage instance', function(){
+      let storage = new Storage('fileSystem', newStorage, customStoragesMap)
+      expect(storage.STORAGES_MAP).to.have.property('fileSystem')       
+    })
+  })
 });
