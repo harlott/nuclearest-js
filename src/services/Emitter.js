454578f1778dd6d7e2fccb2acfc44744af71d20f
harlott <marcoallott@gmail.com>
2017-12-07 07:44:40 +0100

    add event emitter singleton

---
+++ src/services/Emitter.js
@@ -0,0 +1,25 @@
+let EventEmitter = require('events').EventEmitter
+
+let _instance = null
+export default class Emitter{
+    constructor(){
+      this.eventEm = new EventEmitter();
+      if (_instance === null){
+          _instance = this
+      }
+
+      return _instance
+    }
+    emitGeneric(eventType, resObj){
+        this.eventEm.emit(eventType, resObj);
+    }
+    on(eventType, callback){
+        return this.eventEm.on(eventType, callback);
+    }
+    removeListener(eventType, callback){
+        return this.eventEm.removeListener(eventType, callback);
+    }
+    removeAllListeners(eventType){
+        return this.eventEm.removeAllListeners(eventType);
+    }
+}
