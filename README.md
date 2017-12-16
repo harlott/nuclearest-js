223eca46c237b01bbdb34ff7d5bd5e979912b749
harlott <marcoallott@gmail.com>
2017-12-16 09:31:05 +0100

    fix auth docs

--- README.md
+++ README.md
@@ -149,7 +149,92 @@ Example:
 
 ```
 
+
+#### Authentication: refresh token processing
+
+This is an utility to handle refresh authentication token processing.
+The first unauthorized call pauses all the following, waiting for receiving new token object
+Using es7 async await
+
+Example
+
+```
+   import Storage, {STORAGE_TYPES} from 'nuclearest-js/Storage'
+   import fetch from 'nuclearest-js/fetch'
+   import Headers, {headersMap} from 'nuclearest-js/Headers'
+
+   // In a real world authData,CookieStorage, Headers and Auth instances are imported from a service
+
+   let authData = {
+     tokenObject:{
+       tokenType: 'Bearer',
+       accessToken: 'a1b2-c3d4-e5f6-g7h8',
+       refreshToken: 'k1k2-j3j4-l5l6-p7p8',
+       expiresIn: '2000',
+       scope: 'user.role'
+     }
+   }
+
+
+
+   const cookieStorage = new Storage(STORAGE_TYPES.COOKIE, window.cookie, undefined, {enabled: true, 'grantedProps':['country'], callbackOnDisabled: () => {alert('COOKIE DISABLED')}})
+
+   let headers = new Headers()
+                    .add()
+                    .oauthToken(authData.tokenObject)
+                    .custom('x-application-id', clientData.applicationId)
+                    .use()
+
+   const refreshTokenApiCall = () => {
+     return fetch('/my-refresh-token', {
+       headers: headers,
+       method: 'POST',
+       body: JSON.stringify({
+           refreshToken: authData.tokenObject.refreshToken
+       })
+     })
+   }
+
+   const confirmAuthenticationCallback = (tokenObject) => {
+     cookieStorage.setItem('tokenObject', tokenObject)
+     authData.tokenObject = Object.assign({}, tokenObject)
+   }
+
+   const resetAuthenticationCallback = () => {
+     cookieStorage.removeItem('tokenObject')
+     location.href = '/login'
+   }
+
+   const getAuthData = () => {
+     return cookieStorage.getItem('tokenObject')
+   }
+
+   const auth = new Auth(refreshTokenApiCall, confirmAuthenticationCallback, resetAuthenticationCallback)
+
+   const getContents = () => {
+       return fetch('/contents', {
+       headers: headers,
+       method: 'GET'
+     })
+   }
+
+   const processContents = async () => {
+      try {
+         const contentsResult = await auth.proxy(getAuthData, getContents)
+         // Do your staff here to handle response
+         const jsonResult = await contentsResult.json()
+         console.log(JSON.stringify(jsonResult))
+      } catch(error){
+         // Do your staff here to handle exceptions
+      }
+   }
+
+   processContents()
+
+```
+
+
+
 #### Next Releases
 - fetch response object: no diff with original fetch   
-- Authentication flow with refresh token
 - Runtime Mocking System
--- src/Auth/index.js
+++ src/Auth/index.js
@@ -1,9 +1,86 @@
-import Emitter from '../services/Emitter'
 import { cloneDeep, isFunction, get, isEmpty } from 'lodash'
 
-
-const eventEmitter = new Emitter()
-
+/**
+ * This is an utility to handle refresh authentication token processing.
+ * The first unauthorized call pauses all the following, waiting for receiving new token object
+ * Using es7 async await
+ * @type {Object}
+ *
+ * @example
+ *
+ *  import Storage, {STORAGE_TYPES} from 'nuclearest-js/Storage'
+ *  import fetch from 'nuclearest-js/fetch'
+ *  import Headers, {headersMap} from 'nuclearest-js/Headers'
+ *
+ *  // In a real world authData,CookieStorage, Headers and Auth instances are imported from a service
+ *
+ *  let authData = {
+ *    tokenObject:{
+ *      tokenType: 'Bearer',
+ *      accessToken: 'a1b2-c3d4-e5f6-g7h8',
+ *      refreshToken: 'k1k2-j3j4-l5l6-p7p8',
+ *      expiresIn: '2000',
+ *      scope: 'user.role'
+ *    }
+ *  }
+ *
+ *
+ *
+ *  const cookieStorage = new Storage(STORAGE_TYPES.COOKIE, window.cookie, undefined, {enabled: true, 'grantedProps':['country'], callbackOnDisabled: () => {alert('COOKIE DISABLED')}})
+ *
+ *  let headers = new Headers()
+ *                   .add()
+ *                   .oauthToken(authData.tokenObject)
+ *                   .custom('x-application-id', clientData.applicationId)
+ *                   .use()
+ *
+ *  const refreshTokenApiCall = () => {
+ *    return fetch('/my-refresh-token', {
+ *      headers: headers,
+ *      method: 'POST',
+ *      body: JSON.stringify({
+ *          refreshToken: authData.tokenObject.refreshToken
+ *      })
+ *    })
+ *  }
+ *
+ *  const confirmAuthenticationCallback = (tokenObject) => {
+ *    cookieStorage.setItem('tokenObject', tokenObject)
+ *    authData.tokenObject = Object.assign({}, tokenObject)
+ *  }
+ *
+ *  const resetAuthenticationCallback = () => {
+ *    cookieStorage.removeItem('tokenObject')
+ *    location.href = '/login'
+ *  }
+ *
+ *  const getAuthData = () => {
+ *    return cookieStorage.getItem('tokenObject')
+ *  }
+ *
+ *  const auth = new Auth(refreshTokenApiCall, confirmAuthenticationCallback, resetAuthenticationCallback)
+ *
+ *  const getContents = () => {
+ *      return fetch('/contents', {
+ *      headers: headers,
+ *      method: 'GET'
+ *    })
+ *  }
+ *
+ *  const processContents = async () => {
+ *     try {
+ *        const contentsResult = await auth.proxy(getAuthData, getContents)
+ *        // Do your staff here to handle response
+ *        const jsonResult = await contentsResult.json()
+ *        console.log(JSON.stringify(jsonResult))
+ *     } catch(error){
+ *        // Do your staff here to handle exceptions
+ *     }
+ *  }
+ *
+ *  processContents()
+ *
+ */
 class Auth{
   constructor(refreshTokenApiCall, confirmAuthenticationCallback, resetAuthenticationCallback, options={beforeRefreshTokenCallback: () => {}}){
     if (isFunction(refreshTokenApiCall) === false){
@@ -127,7 +204,7 @@ class Auth{
       try {
         const errorProcessed = await error
       } catch (err){
-        return
+        return Promise.reject(err)
       }
       return Promise.reject(error)
     }
