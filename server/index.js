f3711eaa2bc1b0bdf2ddf71b5af79eed251bb01c
harlott <marcoallott@gmail.com>
2017-12-27 11:26:01 +0100

    try to fix travis build moving fetch test in another directory

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ var express = require('express'),
   routes = require('./routes')
   port = process.env.PORT || 3000;
 routes(app)
-app.listen(port);
+app.listen(port, (err, data) => {
+  if (err) throw err;
+});
 
 console.log('todo list RESTful API server started on: ' + port);
